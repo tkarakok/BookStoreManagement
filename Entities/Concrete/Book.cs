d823947a20896616f6ccadf49a19079217c92c80
tkarakok <karakoktunahan78@gmail.com>
2023-11-19 16:27:56 +0300

    created entities and book store database

---
+++ Entities/Concrete/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Book : IEntity
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public int CategoryId { get; set; }
+        public int AuthorId { get; set; }
+        public int UnitsInStock { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+    }
+}
